962219fb2537fffcd93fbb19e3a6cc045b0c3205
tehnn <tehnnn@gmail.com>
2016-12-29 13:22:29 +0700

    utehn-smartcare

--- frontend/modules/test/views/default/cal.php
+++ frontend/modules/test/views/default/cal.php
@@ -1,30 +1,44 @@
 <?php
+
 $events = array();
-  //Testing
-  $Event = new \yii2fullcalendar\models\Event();
-  $Event->id = 1;
-  $Event->title = 'Testing';
-  $Event->start = date('Y-m-d\TH:i:s\Z');
-  $events[] = $Event;
+//Testing
+$Event = new \yii2fullcalendar\models\Event();
+$Event->id = 1;
+$Event->title = 'เปลี่ยนผ้าอ้อม';
+$Event->start = date('Y-m-d\TH:i:s\Z');
+$events[] = $Event;
 
-  $Event = new \yii2fullcalendar\models\Event();
-  $Event->id = 2;
-  $Event->title = 'Testing';
-  $Event->start = date('Y-m-d\TH:i:s\Z',strtotime('tomorrow 6am'));
-  $events[] = $Event;
+$Event = new \yii2fullcalendar\models\Event();
+$Event->id = 2;
+$Event->title = 'ทำความสะอาดสายยางให้อาหาร';
+$Event->start = '2016-12-31 08:00:00';
+;
+$events[] = $Event;
 
-  ?>
+$Event = new \yii2fullcalendar\models\Event();
+$Event->id = 3;
+$Event->title = 'ทำความสะอาดสายยางให้อาหาร';
+$Event->start = '2016-12-30 08:00';
+$Event->end = '2016-12-30 09:00';
+$events[] = $Event;
+?>
 
-  <?= \yii2fullcalendar\yii2fullcalendar::widget(array(
-      'events'=> $events,
-      'options' => [
-        'lang' => 'th', 
-          'id'=>'cal1'
-        //'firstDay'=>'3'
-      ],
-      'clientOptions'=>[
-          'firstDay'=>'1'
-      ]
-  ));
-  
-  
+<?=
+
+\yii2fullcalendar\yii2fullcalendar::widget(array(
+    'events' => $events,
+    'options' => [
+        'lang' => 'th',
+        'id' => 'cal1'
+    ],
+     'header' => [
+      'center' => 'title',
+      'right' => 'month,agendaWeek,listWeek,listDay',
+      'left' => 'prev,next today'
+      ], 
+    'clientOptions' => [
+        'firstDay' => '1',
+    //'defaultView'=>'listWeek',
+    ]
+));
+
