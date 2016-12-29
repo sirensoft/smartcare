27ca724175f68bb6b180875961ea6f397a065a5c
tehnn <tehnnn@gmail.com>
2016-12-29 13:29:04 +0700

    utehn-smartcare

--- frontend/modules/test/views/default/cal.php
+++ frontend/modules/test/views/default/cal.php
@@ -5,14 +5,13 @@ $events = array();
 $Event = new \yii2fullcalendar\models\Event();
 $Event->id = 1;
 $Event->title = 'เปลี่ยนผ้าอ้อม';
-$Event->start = date('Y-m-d\TH:i:s\Z');
+$Event->start = '2016-11-20 09:00';
 $events[] = $Event;
 
 $Event = new \yii2fullcalendar\models\Event();
 $Event->id = 2;
 $Event->title = 'ทำความสะอาดสายยางให้อาหาร';
 $Event->start = '2016-12-31 08:00:00';
-;
 $events[] = $Event;
 
 $Event = new \yii2fullcalendar\models\Event();
@@ -20,6 +19,13 @@ $Event->id = 3;
 $Event->title = 'ทำความสะอาดสายยางให้อาหาร';
 $Event->start = '2016-12-30 08:00';
 $Event->end = '2016-12-30 09:00';
+$events[] = $Event;
+
+$Event = new \yii2fullcalendar\models\Event();
+$Event->id = 4;
+$Event->title = 'ทำความสะอาดสายยางให้อาหาร';
+$Event->start = '2016-12-30 09:00';
+$Event->end = '2016-12-30 10:00';
 $events[] = $Event;
 ?>
 
