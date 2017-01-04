b6003bc78997008f852f7df2c9f9039ae519a10f
tehnn <tehnnn@gmail.com>
2017-01-04 16:37:58 +0700

    utehn-smartcare

--- common/components/MyHelper.php
+++ common/components/MyHelper.php
@@ -103,7 +103,7 @@ class MyHelper extends \yii\base\Component {
     }
     
     public static function setPatientADL($pid=NULL){
-       $sql = "select adl from assessment where patient_id = $pid and (adl is not null and adl != '') order by id DESC limit 1";
+       $sql = "select adl_score from assessment where patient_id = $pid and (adl_score is not null and adl_score != '') order by id DESC limit 1";
        $adl = \Yii::$app->db->createCommand($sql)->queryScalar();
        $sql = "update patient set adl='$adl' where id=$pid";
        return \Yii::$app->db->createCommand($sql)->execute();
@@ -111,7 +111,7 @@ class MyHelper extends \yii\base\Component {
     }
     
     public static function setPatientTAI($pid=NULL){
-       $sql = "select tai from assessment where patient_id = $pid and (tai is not null and tai != '') order by id DESC limit 1";
+       $sql = "select tai_class from assessment where patient_id = $pid and (tai_class is not null and tai_class != '') order by id DESC limit 1";
        $tai = \Yii::$app->db->createCommand($sql)->queryScalar();
        $sql = "update patient set tai='$tai' where id=$pid";
        return \Yii::$app->db->createCommand($sql)->execute(); 
