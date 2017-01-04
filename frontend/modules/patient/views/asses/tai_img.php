2f059f3208c90eb7acbdfd45ae2fbc3ea99e6a1e
tehnn <tehnnn@gmail.com>
2017-01-04 20:54:22 +0700

    utehn-smartcare

--- frontend/modules/patient/views/asses/tai_img.php
+++ frontend/modules/patient/views/asses/tai_img.php
@@ -6,34 +6,40 @@ use common\components\MyHelper;
 use kartik\select2\Select2;
 ?>
 <div class="row">
-    <div class="col-sm-6">
+    <div class="col-lg-6">
         <?= Html::img('./web_img/tai.png'); ?>
     </div>
-    <div class="col-sm-6">
+    <div class="col-lg-6">
 
         <form method="POST">
-            <div class="form-group">
-                <label for="adl_score">ผลประเมิน ADL:</label>
-                <input type="text" class="form-control" id="adl_score" name="adl_score" placeholder="ค่าจากประเมิน ADL">
-            </div>
-            <div class="form-group">
-                <label for="tai_class">ผลประเมิน TAI:</label>
-                <?php
-                $items = MyHelper::dropDownItems("select id,val from c_tai", 'id', 'val');
-                ?>
+            <div class="row">
+                <div class="form-group col-lg-4" >
+                    <label for="adl_score">ผลประเมิน ADL:</label>
+                    <input type="text" class="form-control" id="adl_score" name="adl_score" placeholder="ค่าจากประเมิน ADL">
+                </div>
+                <div class="form-group col-lg-4" >
+                    <label for="tai_class">ผลประเมิน TAI:</label>
+                    <?php
+                    $items = MyHelper::dropDownItems("select id,val from c_tai", 'id', 'val');
+                    ?>
 
-                <?php
-                echo Select2::widget([
-                    'name' => 'tai_class',
-                    'data' => $items,
-                    'options' => ['placeholder' => 'เลือก...'],
-                    'pluginOptions' => [
-                        'allowClear' => true
-                    ],
-                ]);
-                ?>
+                    <?php
+                    echo Select2::widget([
+                        'name' => 'tai_class',
+                        'data' => $items,
+                        'options' => ['placeholder' => 'เลือก...'],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                    ]);
+                    ?>
+                </div>
             </div>
-            <button type="submit" class="btn btn-default"> บันทึกผล </button>
+            <div class="row">
+                <div class="form-group col-lg-4" >
+                    <button type="submit" class="btn btn-default"> บันทึกผล </button>
+                </div>
+            </div>
         </form>
 
 
