bdbe229ef424a0ba3abf7cceb8b4fd24a4591adb
tehnn <tehnnn@gmail.com>
2017-01-10 18:42:19 +0700

    utehn-smartcare

--- frontend/models/PlanSearch.php
+++ frontend/models/PlanSearch.php
@@ -47,6 +47,7 @@ class PlanSearch extends Plan
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort'=> ['defaultOrder' => ['id'=>SORT_DESC]]
         ]);
 
         $this->load($params);
