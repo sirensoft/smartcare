96e4848f49ccdf82857d0dc67635083157a6a6f3
tehnn <tehnnn@gmail.com>
2017-01-10 19:34:42 +0700

    utehn-smartcare

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -11,38 +11,56 @@ use yii\widgets\ActiveForm;
 <div class="user-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <div class="form-group">
+        <div class="col-md-3">
+            <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'password_hash')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'status')->textInput() ?>
+        </div>
+    </div>
 
-    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+    <div class="form-group">
+        <div class="col-md-6">
+            <?= $form->field($model, 'role')->textInput() ?>
+        </div>
+        <div class="col-md-6">
+            <?= $form->field($model, 'role_name')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
 
-  
+    <div class="form-group">
+        <div class="col-md-3">
+            <?= $form->field($model, 'u_cid')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'u_prename')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'u_name')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'u_lname')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-md-4">
+            <?= $form->field($model, 'office')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'officer_type')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'office_position')->textInput(['maxlength' => true]) ?>
+        </div>
 
-    <?= $form->field($model, 'password_hash')->textInput(['maxlength' => true]) ?>
-
-  
-
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'status')->textInput() ?>
-
-    <?= $form->field($model, 'role')->textInput() ?>
-
-    <?= $form->field($model, 'role_name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'u_cid')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'u_prename')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'u_name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'u_lname')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'office')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'officer_type')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'office_position')->textInput(['maxlength' => true]) ?>
-
-    
+    </div>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
