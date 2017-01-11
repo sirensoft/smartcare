d03007d0b3eae6c0c66d1e2e86582c1966b0d83c
tehnn <tehnnn@gmail.com>
2017-01-11 14:42:32 +0700

    utehn-smartcare

--- frontend/modules/care/views/plan/update.php
+++ frontend/modules/care/views/plan/update.php
@@ -1,14 +1,15 @@
 <?php
 
 use yii\helpers\Html;
+use common\components\MyHelper;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Plan */
 
 $this->title = 'แก้ไขแผน: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Plans', 'url' => ['index','pid'=>$model->patient_id]];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = ['label' => 'รายการแผน', 'url' => ['index','pid'=>$model->patient_id]];
+$this->params['breadcrumbs'][] = ['label' => MyHelper::ptInfo_($model->patient_id), 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'แก้ไข';
 ?>
 <div class="plan-update">
 
