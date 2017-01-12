3d51ea9dade6bea0cf670ee424c2beb715a017e7
tehnn <tehnnn@gmail.com>
2017-01-12 09:18:24 +0700

    utehn-smartcare

---
+++ common/config/con_db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=smartcare;port=3309',
+    'username' => 'root',
+    'password' => '1234',
+    'charset' => 'utf8',
+];
+
---
+++ common/config/con_hdc.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=61.19.22.203;dbname=dhdc;port=3306',
+    'username' => 'ltc',
+    'password' => 'ltc',
+    'charset' => 'utf8',
+];
+
