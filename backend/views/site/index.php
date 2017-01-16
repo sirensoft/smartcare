60ee28524c0cc2126488bc3b7f102e38161d9fce
tehnn <tehnnn@gmail.com>
2017-01-16 10:12:22 +0700

    utehn-smartcare

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,10 +1,16 @@
 <?php
-
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 
 $this->title = '::Backend::';
 ?>
 <div class="site-index">
-
+   
+    <div class="row">
+        <div class="col-lg-6">
+            <?=Html::a('<i class="glyphicon glyphicon-user"></i> จัดการผู้ใช้',['/user/'],['class'=>'btn btn-lg btn-danger'])?>            
+        </div>
+        
+    </div>
    
 </div>
