16ff4aaececc73350eea4b9a752e122fe551a047
tehnn <tehnnn@gmail.com>
2017-01-22 09:30:23 +0700

    utehn-smartcare

--- frontend/modules/map/views/default/index.php
+++ frontend/modules/map/views/default/index.php
@@ -4,9 +4,16 @@ $this->registerCssFile('//api.mapbox.com/mapbox.js/v3.0.1/mapbox.css', ['async' 
 $this->registerCssFile('./lib-gis/leaflet-search.min.css',['async' => false, 'defer' => true]);
 $this->registerCssFile('./lib-gis/leaflet.label.css',['async' => false, 'defer' => true]);
 
+$this->registerCssFile('//domoritz.github.io/leaflet-locatecontrol/dist/L.Control.Locate.min.css', ['async' => false, 'defer' => true]);
+$this->registerCssFile('//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', ['async' => false, 'defer' => true]);
+
 $this->registerJsFile('//api.mapbox.com/mapbox.js/v3.0.1/mapbox.js', ['position' => $this::POS_HEAD]);
 $this->registerJsFile('./lib-gis/leaflet-search.min.js',['position' => $this::POS_HEAD]);
 $this->registerJsFile('./lib-gis/leaflet.label.js',['position' => $this::POS_HEAD]);
+
+$this->registerJsFile('//domoritz.github.io/leaflet-locatecontrol/dist/L.Control.Locate.min.js', ['position' => $this::POS_HEAD]);
+
+
 ?>
 
 
@@ -30,6 +37,7 @@ $js = <<<JS
             
 L.mapbox.accessToken = 'pk.eyJ1IjoidGVobm5uIiwiYSI6ImNpZzF4bHV4NDE0dTZ1M200YWxweHR0ZzcifQ.lpRRelYpT0ucv1NN08KUWQ';
 var map = L.mapbox.map('map').setView([16.74094277,100.27255121], 9); 
+L.control.locate().addTo(map);
 var baseLayers = {
 	"แผนที่ถนน": L.mapbox.tileLayer('mapbox.streets').addTo(map),        
         "แผนที่ดาวเทียม": L.mapbox.tileLayer('mapbox.satellite'),
