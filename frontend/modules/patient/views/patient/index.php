a5bd796e17aa855e141ef0ae952c240b9ede9274
tehnn <tehnnn@gmail.com>
2017-01-26 22:25:10 +0700

    utehn-smartcare

--- frontend/modules/patient/views/patient/index.php
+++ frontend/modules/patient/views/patient/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <?= Html::a('<i class="glyphicon glyphicon-map-marker"></i> แผนที่', ['/map'], ['class' => 'btn btn-warning', 'target' => '_blank']) ?>
             <?= Html::a('<i class="glyphicon glyphicon-plus"></i> เพิ่ม', ['create'], ['class' => 'btn btn-success']) ?>
         
-            <?= Html::a('<i class="glyphicon glyphicon-user"></i> จำหน่ายแล้ว', ['index-discharge'], ['class' => 'btn btn-default pull-right','target'=>'_blank']) ?>
+            <?= Html::a('<i class="glyphicon glyphicon-user"></i> จำหน่ายแล้ว', ['index-discharge'], ['class' => 'btn btn-default pull-right']) ?>
 
     <?php endif; ?>
     </p>
