de51652aead32eff3514873b15551b2801978fcc
tehnn <tehnnn@gmail.com>
2017-02-01 17:44:41 +0700

    utehn-smartcare

--- frontend/modules/care/views/visit/create.php
+++ frontend/modules/care/views/visit/create.php
@@ -8,8 +8,9 @@ use common\components\MyHelper;
 /* @var $model frontend\models\Visit */
 
 $this->title = 'เพิ่มการเยี่ยม: '.MyHelper::ptInfo_($model->patient_id);
+$this->params['breadcrumbs'][] = ['label' => MyHelper::ptInfo_($model->patient_id), 'url' =>['/patient/patient/view','pid'=>$model->patient_id]];
 $this->params['breadcrumbs'][] = ['label' => 'รายการเยี่ยม', 'url' => ['index','pid'=>$model->patient_id]];
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = 'เพิ่มการเยี่ยม';
 ?>
 <div class="visit-create">
 
--- frontend/modules/care/views/visit/update.php
+++ frontend/modules/care/views/visit/update.php
@@ -1,11 +1,13 @@
 <?php
 
 use yii\helpers\Html;
+use common\components\MyHelper;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Visit */
 
 $this->title = 'แก้ไข เยี่ยมวันที่: ' . $model->date_visit;
+$this->params['breadcrumbs'][] = ['label' => MyHelper::ptInfo_($model->patient_id), 'url' =>['/patient/patient/view','pid'=>$model->patient_id]];
 $this->params['breadcrumbs'][] = ['label' => 'รายการเยี่ยม', 'url' => ['index','pid'=>$model->patient_id]];
 //$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'แก้ไขการเยี่ยม';
