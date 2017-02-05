c67bf1f0ccad6132caef798c5a7169f56472e58f
tehnn <tehnnn@gmail.com>
2017-02-05 14:08:14 +0700

    utehn-smartcare

--- common/config/con_hdc.php
+++ common/config/con_hdc.php
@@ -2,7 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=61.19.22.203;dbname=dhdc;port=3306',
+    //'dsn' => 'mysql:host=61.19.22.203;dbname=dhdc;port=3306',
+    'dsn' => 'mysql:host=plk.hdc.moph.go.th;dbname=hdc;port=3306',
     'username' => 'ltc',
     'password' => 'ltc',
     'charset' => 'utf8',
