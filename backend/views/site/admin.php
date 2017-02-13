9489cd5b62a319078f56f8ee5dc8c20ead07e7d4
tehnn <tehnnn@gmail.com>
2017-02-13 08:29:53 +0700

    utehn-smartcare

--- backend/views/site/admin.php
+++ backend/views/site/admin.php
@@ -1,13 +1,20 @@
 <?php
-use yii\helpers\Html;
 
+use yii\helpers\Html;
+use frontend\modules\ehr\models\OnOffEhr;
+
+$mEhr = OnOffEhr::find()->one();
 ?>
 <div class="row">
     <div class="col-md-4">
-        <?= Html::a('<i class="glyphicon glyphicon-user"></i> จัดการผู้ใช้', ['/user/'], ['class' => 'btn btn-lg btn-success']) ?>            
+        <?= Html::a('<i class="glyphicon glyphicon-user"></i> USER', ['/user/'], ['class' => 'btn btn-lg btn-primary']) ?>            
     </div>
     <div col-md-4>
-        <?= Html::a('<i class="glyphicon glyphicon-off"></i> On/Off EHR', ['/site/ehr'], ['class' => 'btn btn-lg btn-danger']) ?>
+        <?php if ($mEhr->status == 'on'): ?>            
+            <?= Html::a('<i class="glyphicon glyphicon-off"></i> EHR', ['/site/ehr'], ['class' => 'btn btn-lg btn-success']) ?>
+        <?php else : ?>
+             <?= Html::a('<i class="glyphicon glyphicon-off"></i> EHR', ['/site/ehr'], ['class' => 'btn btn-lg btn-danger']) ?>
+        <?php endif; ?>
     </div>
 
 </div>
