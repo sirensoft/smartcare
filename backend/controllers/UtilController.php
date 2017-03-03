76a9bc2f2d4981477a64951a1bed78696aa2fb03
tehnn <tehnnn@gmail.com>
2017-03-03 10:58:40 +0700

    ems

--- backend/controllers/UtilController.php
+++ backend/controllers/UtilController.php
@@ -4,7 +4,9 @@ namespace backend\controllers;
 
 use common\components\AppController;
 
-
+use frontend\models\Person;
+use frontend\models\FilePerson;
+use common\components\MyHelper;
 
 class UtilController extends AppController
 {
@@ -19,6 +21,24 @@ class UtilController extends AppController
     }
     public function actionProcessList(){
         return $this->render('process-list');
+    }
+    
+    public function actionPerson() {
+
+        $hospcode = MyHelper::getUserOffice();
+        $oModel = Person::findAll(['HOSPCODE' => $hospcode]);
+
+        foreach ($oModel as $obj) {
+            $clone = new FilePerson();
+            $clone->attributes = $obj->attributes;
+
+            try {
+                $clone->save();
+            } catch (\yii\db\Exception $e) {
+                $clone->isNewRecord = false;
+                $clone->save();
+            }
+        }
     }
 
 }
