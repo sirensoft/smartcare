ad1f2c028ea17982cb4a7c5856c6899643b4876c
tehnn <tehnnn@gmail.com>
2017-03-03 10:34:32 +0700

    ems

--- frontend/controllers/Test2Controller.php
+++ frontend/controllers/Test2Controller.php
@@ -1,24 +1,35 @@
 <?php
 
 namespace frontend\controllers;
-use frontend\models\Person;
 
-class Test2Controller extends \yii\web\Controller
-{
-    public function actionIndex()
-    {
+use frontend\models\Person;
+use frontend\models\FilePerson;
+
+class Test2Controller extends \yii\web\Controller {
+
+    public function actionIndex() {
         $sql = " SELECT t.rapid_code FROM plan t  WHERE  t.patient_id = '2'
                 ORDER BY t.id DESC LIMIT 1 ";
         $color = \Yii::$app->db->createCommand($sql)->queryScalar();
         echo $color;
     }
-    
-    public function actionTestTime(){
+
+    public function actionTestTime() {
         return date('Y-m-d H:i:s');
     }
+
     public function actionPerson() {
-        $models = Person::find()->where(['HOSPCODE'=>'07552','CID'=>'1650501125470'])->all();
-        var_dump($models);
+
+        $obj = Person::findOne(['HOSPCODE' => '07552']);
+
+        $clone = new FilePerson();
+        $clone->attributes = $obj->attributes;
+        try {
+            $clone->save();
+        } catch (\yii\db\Exception $e) {
+            $clone->isNewRecord = false;
+            $clone->save();
+        }
     }
 
 }
